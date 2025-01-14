9e6ce869a1949a82481dd5d35749c5eb00e26740
Nathan <nathan@Nathans-MBP.lan>
2025-01-14 14:02:59 -0500

    changes

--- android/breathingmonitor2/app/src/main/java/com/example/breathingmonitor/MainActivity.kt
+++ android/breathingmonitor2/app/src/main/java/com/example/breathingmonitor/MainActivity.kt
@@ -10,10 +10,8 @@ import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
-import java.io.BufferedReader
 import java.io.DataInputStream
 import java.io.InputStream
-import java.io.InputStreamReader
 import java.net.Socket
 import kotlin.concurrent.thread
 
